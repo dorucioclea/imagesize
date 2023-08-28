e3d55f50b34c52c6503b246d671d3109ea446e40
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-08-28 15:36:42 +0300

    style

--- ImageSize/ImageSize/Extensions.cs
+++ ImageSize/ImageSize/Extensions.cs
@@ -11,13 +11,13 @@ public static class Extensions
     public static int ReadInt32BigEndian(this BinaryReader reader)
     {
         var bytes = reader.ReadBytes(4);
-        return (int)((bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | bytes[3]);
+        return (bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | bytes[3];
     }
     
     public static int ReadInt32LittleEndian(this BinaryReader reader)
     {
         var bytes = reader.ReadBytes(4);
-        return (int)((bytes[0]) | (bytes[1] << 8) | (bytes[2] << 16) | (bytes[3] << 24));
+        return (bytes[0]) | (bytes[1] << 8) | (bytes[2] << 16) | (bytes[3] << 24);
     }
     
     public static short ReadInt16LittleEndian(this BinaryReader reader)
