63a11293c7d827ed338287fd47b06af3ec00cdc9
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-08-28 15:57:35 +0300

    feat: image size tests for jpg

---
+++ ImageSize/ImageSize.Tests/ImageSizeTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace ImageSize.Tests;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public class ImageSizeTests
+{
+    [Fact]
+    public void TestJPEGSize()
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "200x300.jpg");
+        using var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+        var size = ImageSize.GetImageSize(fileStream);
+        
+        Assert.NotNull(size);
+        Assert.Equal(200, size.Value.width);
+        Assert.Equal(300, size.Value.height);
+    }
+}
