87f19b4b7f7ab194411a7a63169107777766fa4a
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-08-28 15:45:26 +0300

    style: cleanup test methods

--- ImageSize/ImageSize.Tests/ExtensionsTests.cs
+++ ImageSize/ImageSize.Tests/ExtensionsTests.cs
@@ -8,47 +8,39 @@ public class ExtensionsTests
     public void TestReadInt16BigEndian()
     {
         var data = new byte[] { 0x12, 0x34 };
-        using (var stream = new MemoryStream(data))
-        using (var reader = new BinaryReader(stream))
-        {
-            short result = reader.ReadInt16BigEndian();
-            Assert.Equal(0x1234, result);
-        }
+        using var stream = new MemoryStream(data);
+        using var reader = new BinaryReader(stream);
+        var result = reader.ReadInt16BigEndian();
+        Assert.Equal(0x1234, result);
     }
 
     [Fact]
     public void TestReadInt32BigEndian()
     {
         var data = new byte[] { 0x12, 0x34, 0x56, 0x78 };
-        using (var stream = new MemoryStream(data))
-        using (var reader = new BinaryReader(stream))
-        {
-            int result = reader.ReadInt32BigEndian();
-            Assert.Equal(0x12345678, result);
-        }
+        using var stream = new MemoryStream(data);
+        using var reader = new BinaryReader(stream);
+        var result = reader.ReadInt32BigEndian();
+        Assert.Equal(0x12345678, result);
     }
 
     [Fact]
     public void TestReadInt16LittleEndian()
     {
         var data = new byte[] { 0x34, 0x12 };
-        using (var stream = new MemoryStream(data))
-        using (var reader = new BinaryReader(stream))
-        {
-            short result = reader.ReadInt16LittleEndian();
-            Assert.Equal(0x1234, result);
-        }
+        using var stream = new MemoryStream(data);
+        using var reader = new BinaryReader(stream);
+        var result = reader.ReadInt16LittleEndian();
+        Assert.Equal(0x1234, result);
     }
 
     [Fact]
     public void TestReadInt32LittleEndian()
     {
         var data = new byte[] { 0x78, 0x56, 0x34, 0x12 };
-        using (var stream = new MemoryStream(data))
-        using (var reader = new BinaryReader(stream))
-        {
-            int result = reader.ReadInt32LittleEndian();
-            Assert.Equal(0x12345678, result);
-        }
+        using var stream = new MemoryStream(data);
+        using var reader = new BinaryReader(stream);
+        var result = reader.ReadInt32LittleEndian();
+        Assert.Equal(0x12345678, result);
     }
 }
