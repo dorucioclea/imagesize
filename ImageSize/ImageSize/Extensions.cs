2272880489d4fb5d1db425d31d72b429136d667d
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-08-28 16:08:55 +0300

    feat: rework tiff image format

--- ImageSize/ImageSize/Extensions.cs
+++ ImageSize/ImageSize/Extensions.cs
@@ -25,4 +25,22 @@ public static class Extensions
         var bytes = reader.ReadBytes(2);
         return (short)((bytes[0]) | (bytes[1] << 8));
     }
+    
+    public static ushort ReadUInt16BigEndian(this BinaryReader reader)
+    {
+        var bytes = reader.ReadBytes(2);
+        if (bytes.Length < 2)
+            throw new EndOfStreamException();
+
+        return (ushort)((bytes[0] << 8) | bytes[1]);
+    }
+    
+    public static uint ReadUInt32BigEndian(this BinaryReader reader)
+    {
+        var bytes = reader.ReadBytes(4);
+        if (bytes.Length < 4)
+            throw new EndOfStreamException();
+
+        return ((uint)bytes[0] << 24) | ((uint)bytes[1] << 16) | ((uint)bytes[2] << 8) | bytes[3];
+    }
 }
--- ImageSize/ImageSize/ImageSize.cs
+++ ImageSize/ImageSize/ImageSize.cs
@@ -5,7 +5,7 @@ public static class ImageSize
     public static (int width, int height)? GetImageSize(Stream imageStream)
     {
         using var binaryReader = new BinaryReader(imageStream);
-        
+
         var header = binaryReader.ReadBytes(8);
 
         // Check for JPEG magic numbers
@@ -22,7 +22,7 @@ public static class ImageSize
             var height = binaryReader.ReadInt32BigEndian();
             return (width, height);
         }
-                
+
         // Check for GIF magic numbers
         else if (header[0] == 0x47 && header[1] == 0x49 && header[2] == 0x46)
         {
@@ -31,7 +31,7 @@ public static class ImageSize
             int height = binaryReader.ReadInt16LittleEndian();
             return (width, height);
         }
-        
+
         // Check for BMP magic numbers
         else if (header[0] == 0x42 && header[1] == 0x4D)
         {
@@ -40,9 +40,10 @@ public static class ImageSize
             var height = binaryReader.ReadInt32LittleEndian();
             return (width, height);
         }
-        
+
         // Check for TIFF magic numbers
-        else if (header.Take(2).SequenceEqual(new byte[] { 0x49, 0x49 }) || header.Take(2).SequenceEqual(new byte[] { 0x4D, 0x4D }))  // "II" or "MM"
+        else if (header.Take(2).SequenceEqual(new byte[] {0x49, 0x49}) ||
+                 header.Take(2).SequenceEqual(new byte[] {0x4D, 0x4D})) // "II" or "MM"
         {
             bool isLittleEndian = header[0] == 0x49;
             binaryReader.BaseStream.Seek(4, SeekOrigin.Current);
@@ -53,42 +54,72 @@ public static class ImageSize
             return (width, height);
         }
 
-        
-        // // Check for PNG magic numbers
-        // if (header.SequenceEqual(new byte[] { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A }))
-        // {
-        //     binaryReader.ReadBytes(8);  // Skip remaining header
-        //     return (binaryReader.ReadInt32BigEndian(), binaryReader.ReadInt32BigEndian());
-        // }
-        //
-        // // Check for GIF magic numbers
-        // else if (header.Take(3).SequenceEqual(new byte[] { 0x47, 0x49, 0x46 }))  // "GIF"
-        // {
-        //     return (binaryReader.ReadInt16LittleEndian(), binaryReader.ReadInt16LittleEndian());
-        // }
-        //
-        // // Check for BMP magic numbers
-        // else if (header.Take(2).SequenceEqual(new byte[] { 0x42, 0x4D }))  // "BM"
-        // {
-        //     binaryReader.BaseStream.Seek(16, SeekOrigin.Current);
-        //     return (binaryReader.ReadInt32LittleEndian(), binaryReader.ReadInt32LittleEndian());
-        // }
-        //
-        // // Check for TIFF magic numbers
-        // else if (header.Take(2).SequenceEqual(new byte[] { 0x49, 0x49 }) || header.Take(2).SequenceEqual(new byte[] { 0x4D, 0x4D }))  // "II" or "MM"
-        // {
-        //     bool isLittleEndian = header[0] == 0x49;
-        //     binaryReader.BaseStream.Seek(4, SeekOrigin.Current);
-        //     int offset = isLittleEndian ? binaryReader.ReadInt32LittleEndian() : binaryReader.ReadInt32BigEndian();
-        //     binaryReader.BaseStream.Seek(offset + 2, SeekOrigin.Begin);
-        //     int width = isLittleEndian ? binaryReader.ReadInt16LittleEndian() : binaryReader.ReadInt16BigEndian();
-        //     int height = isLittleEndian ? binaryReader.ReadInt16LittleEndian() : binaryReader.ReadInt16BigEndian();
-        //     return (width, height);
-        // }
-
         return null;
     }
-    
+
+    private static (int width, int height)? GetTiffSize(BinaryReader reader)
+    {
+        // Read byte order marks
+        var byteOrder = reader.ReadUInt16();
+
+        bool isLittleEndian;
+        if (byteOrder == 0x4949) // II for Intel order (Little-Endian)
+        {
+            isLittleEndian = true;
+        }
+        else if (byteOrder == 0x4D4D) // MM for Motorola order (Big-Endian)
+        {
+            isLittleEndian = false;
+        }
+        else
+        {
+            return null; // Not a valid TIFF file
+        }
+
+        // Read the TIFF magic number (should be 42)
+        var magicNumber = isLittleEndian ? reader.ReadUInt16() : reader.ReadUInt16BigEndian();
+        if (magicNumber != 42)
+        {
+            return null; // Not a valid TIFF file
+        }
+
+        // Read the offset to the first IFD
+        var ifdOffset = isLittleEndian ? reader.ReadUInt32() : reader.ReadUInt32BigEndian();
+
+        reader.BaseStream.Seek(ifdOffset, SeekOrigin.Begin);
+
+        var entryCount = isLittleEndian ? reader.ReadUInt16() : reader.ReadUInt16BigEndian();
+
+        int width = 0, height = 0;
+
+        for (int i = 0; i < entryCount; i++)
+        {
+            var tag = isLittleEndian ? reader.ReadUInt16() : reader.ReadUInt16BigEndian();
+            reader.BaseStream.Seek(2, SeekOrigin.Current); // Skip the type, we assume short/int for width/height
+            var numValues = isLittleEndian ? reader.ReadUInt32() : reader.ReadUInt32BigEndian();
+
+            if (tag == 256) // ImageWidth tag
+            {
+                width = numValues <= ushort.MaxValue
+                    ? (isLittleEndian ? reader.ReadUInt16() : reader.ReadUInt16BigEndian())
+                    : (isLittleEndian ? reader.ReadInt32() : reader.ReadInt32BigEndian());
+            }
+            else if (tag == 257) // ImageHeight tag
+            {
+                height = numValues <= ushort.MaxValue
+                    ? (isLittleEndian ? reader.ReadUInt16() : reader.ReadUInt16BigEndian())
+                    : (isLittleEndian ? reader.ReadInt32() : reader.ReadInt32BigEndian());
+            }
+
+            if (width != 0 && height != 0)
+                return (width, height);
+
+            reader.BaseStream.Seek(4, SeekOrigin.Current); // Move to the next tag
+        }
+
+        return (width, height); // Returns width and height even if one of them is zero
+    }
+
     private static (int width, int height)? GetJpegSize(BinaryReader binaryReader)
     {
         while (binaryReader.BaseStream.Position < binaryReader.BaseStream.Length)
@@ -100,7 +131,8 @@ public static class ImageSize
 
             byte segmentType = binaryReader.ReadByte();
 
-            if (segmentType >= 0xC0 && segmentType <= 0xCF && segmentType != 0xC4 && segmentType != 0xC8 && segmentType != 0xCC)
+            if (segmentType >= 0xC0 && segmentType <= 0xCF && segmentType != 0xC4 && segmentType != 0xC8 &&
+                segmentType != 0xCC)
             {
                 binaryReader.ReadByte(); // Skip length high byte
                 binaryReader.ReadByte(); // Skip length low byte
